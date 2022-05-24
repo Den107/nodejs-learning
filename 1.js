6adf04f929a3560ad3be2152d2b3507d0907c6e5
Denis Kravchuk <deniskrv@gmail.com>
2022-05-24 21:16:44 +0300

    сделал дз2

--- 1.js
+++ 1.js
@@ -1,35 +1,54 @@
-const { green, yellow, red } = require("colors/safe");
+const EventManager = require('events')
+const event = new EventManager()
 
-const isPrime = (number) => {
-  if (number < 2) return false;
+//вытаскиваем из строки нужные данные
+const [hour, day, mount, year] = process.argv[2].split('-')
 
-  for (let i = 2; i <= number / 2; i++) {
-    if (number % i === 0) return false;
-  }
-
-  return true;
-};
-
-
-let count = 1;
-
-const from = process.argv[2];
-const to = process.argv[3];
-
-for (let number = from; number <= to; number++) {
-  let colorer = green;
-
-  if (isPrime(number)) {
-    if (count % 2 === 0) {
-      colorer = yellow;
-      count ++;
-    } else if (count % 3 === 0) {
-      colorer = red;
-      count = 1;
-    } else {
-      count ++;
-    }
-
-    console.log(colorer(number));
-  }
+/**
+ * делаем правильную строку по формату ISO
+ * @returns {string}
+ */
+function createTimeString() {
+    return `${year}-${mount}-${day}T${hour}:00:00`
 }
+
+/**
+ * считаем сколько осталось времени
+ * @param endtime
+ * @returns {string}
+ */
+function getTimeRemaining(endtime) {
+    if ((Date.parse(endtime) === Date.parse(new Date())) || (Date.parse(endtime) < Date.parse(new Date()))) {
+        console.log('Ваше время вышло')
+        return false
+    } else {
+        const t = Date.parse(endtime) - Date.parse(new Date())
+        const years = Math.floor((t / 1000) / 31536000)
+        const mounts = Math.floor((t / 1000) / 2592000)
+        const days = Math.floor(t / (1000 * 60 * 60 * 24)) - Math.floor(mounts * 30)
+        const hours = Math.floor(t / (1000 * 60 * 60) % 24)
+        const minutes = Math.floor((t / 1000 / 60) % 60)
+        const seconds = Math.floor((t / 1000) % 60)
+
+        return `До конца осталось ${years} лет, ${mounts} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`
+    }
+}
+
+/**
+ * запускаем функцию подсчета
+ */
+function run() {
+    let int
+    while (getTimeRemaining(createTimeString())) {
+        event.on('timer', message => console.log(message))
+        event.emit('timer', getTimeRemaining(createTimeString()))
+        int = setInterval(() => run(), 1000)
+    }
+    clearInterval(int)
+    process.exit()
+}
+
+/**
+ * формат таймера часы-число-месяц-год, напр: 20-24-05-2022
+ */
+run()
